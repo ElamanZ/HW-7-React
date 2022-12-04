a564a585b652bd8d5099ececda9a28b09f53c2aa
ElamanZ <z.elaman699@gmail.com>
2022-12-05 05:51:35 +0600

    HW-7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import TitlePage from "./pages/titlePage/TitlePage";
+import TabloPage from "./pages/tabloPage/TabloPage";
+import ContactsPage from "./pages/contactsPage/ContactsPage";
+import TextPage from "./pages/textPage/TextPage";
+import UsersPage from "./pages/usersPage/UsersPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Menu from "./components/menu/Menu";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Menu/>
+            <Routes>
+                <Route index element={<TitlePage/>}/>
+                <Route path="/tablo" element={<TabloPage/>}/>
+                <Route path="/contacts" element={<ContactsPage/>}/>
+                <Route path="/text" element={<TextPage/>}/>
+                <Route path="/users" element={<UsersPage/>}/>
+            </Routes>
+        </BrowserRouter>
+
+
     </div>
   );
 }
